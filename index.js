6582453d9089800d7ee8ed9feb885c4ef65871a6
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-22 15:35:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // Write your code here!
 const body = document.body;
-const main = document.querySelector('main#main')
+const main = document.getElementById('main')
 main.remove();
 
 const newHeader = document.createElement("h1")
-newHeader.appendChild(h1);
-// const id = document.createElement("id")
-// id.innerHTML = "victory";
-// newHeader.appendChild(id);
-newHeader.getElementById("h1#victory");
+newHeader.setAttribute('id','victory')
 newHeader.innerHTML = `Mohamad is the champion`;
+body.appendChild(newHeader);
