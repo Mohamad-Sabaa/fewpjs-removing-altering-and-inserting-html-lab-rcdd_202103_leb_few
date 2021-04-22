3f7b46af5e67635e018e69a2e7610e7b3cc7fe6a
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-04-22 15:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const body = document.body;
 const main = document.querySelector('main#main')
 main.remove();
 
-const newHeader = document.createElement("h1#victory")
+const newHeader = document.createElement("h1")
 newHeader.appendChild(h1);
-const id = document.createElement("id")
-id.innerHTML = "victory";
-newHeader.appendChild(id);
+// const id = document.createElement("id")
+// id.innerHTML = "victory";
+// newHeader.appendChild(id);
+newHeader.getElementById("h1#victory")
